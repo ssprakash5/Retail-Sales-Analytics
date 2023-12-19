6395421240ebf254d050b557cbd8872f7d49c423
Sri Surya Prakash S <154003057+ssprakash5@users.noreply.github.com>
2023-12-19 23:16:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Load your dataset
-data_df_converted = pd.read_csv("/content/df_converted2.csv")
+data_df_converted = pd.read_csv("/content/drive/MyDrive/df_converted2.csv")
 
 # Selecting specific columns for modeling
 selected_columns = ['Store', 'Type', 'Size', 'Dept', 'IsHoliday',
@@ -41,57 +41,36 @@ xgb_model.fit(X_train, y_train)
 st.title("Weekly Sales Prediction App")
 st.sidebar.header("Input Parameters")
 
-# Sliders for user input
+# Selectbox and Sliders for user input
+user_input = {}
 for col in X_train.columns:
-    if col != 'Weekly_Sales':  # Exclude the target variable from sliders
+    if col in ['Store', 'Type', 'Dept', 'IsHoliday']:
+        unique_values = X_train[col].unique()
+        user_input[col] = st.sidebar.selectbox(col, unique_values)
+    elif col != 'Weekly_Sales':
+        default_val = float(X_train[col].mean())
         min_val = float(X_train[col].min())
         max_val = float(X_train[col].max())
-        default_val = float(X_train[col].mean())  # You can choose any default value here
-        
-        # Generate a unique key using column name and a unique suffix
-        key = f"{col}_slider"
-        param = st.sidebar.slider(col, key=key, min_value=min_val, max_value=max_val, value=default_val)
+        user_input[col] = st.sidebar.slider(col, min_value=min_val, max_value=max_val, value=default_val)
 
-# User input DataFrame with selected columns
-user_input = pd.DataFrame({col: [st.sidebar.slider(col, float(X_train[col].min()), float(X_train[col].max()), float(X_train[col].mean()), key=f"{col}_user_input")] for col in X_train.columns if col != 'Weekly_Sales'})
+# Selecting the model
+selected_model = st.sidebar.selectbox("Select Model", ["Decision Tree", "Random Forest", "XGBoost"])
 
-# Ensure the user_input DataFrame has the same columns as X_train
-user_input = user_input.reindex(columns=X_train.columns, fill_value=0)
+if selected_model == "Decision Tree":
+    model = dt_model
+elif selected_model == "Random Forest":
+    model = rf_model
+else:
+    model = xgb_model
 
-# Prediction and Evaluation
-# Predicting with different models based on user input
-dt_prediction = dt_model.predict(user_input)
-rf_prediction = rf_model.predict(user_input)
-xgb_prediction = xgb_model.predict(user_input)
+# Predict button
+if st.sidebar.button("Predict"):
+    # Convert user input to DataFrame
+    user_input_df = pd.DataFrame([user_input])
+    user_input_df = user_input_df.reindex(columns=X_train.columns, fill_value=0)
 
-# Display predictions
-st.write("\n**Decision Tree Prediction:**", dt_prediction[0])
-st.write("\n**Random Forest Prediction:**", rf_prediction[0])
-st.write("\n**XGBoost Prediction:**", xgb_prediction[0])
+    # Prediction
+    prediction = model.predict(user_input_df)
 
-# Evaluate models on the test set
-test_dt_preds = dt_model.predict(X_test)
-rmse_dt = mean_squared_error(y_test, test_dt_preds, squared=False)
-r2_dt = r2_score(y_test, test_dt_preds)
-
-test_rf_preds = rf_model.predict(X_test)
-rmse_rf = mean_squared_error(y_test, test_rf_preds, squared=False)
-r2_rf = r2_score(y_test, test_rf_preds)
-
-test_xgb_preds = xgb_model.predict(X_test)
-rmse_xgb = mean_squared_error(y_test, test_xgb_preds, squared=False)
-r2_xgb = r2_score(y_test, test_xgb_preds)
-
-# Display metrics for different models on the test set
-st.write("\n**Decision Tree Metrics - Test Set:**")
-st.write(f"Root Mean Squared Error: {rmse_dt}")
-st.write(f"R-Squared Score: {r2_dt}")
-
-st.write("\n**Random Forest Metrics - Test Set:**")
-st.write(f"Root Mean Squared Error: {rmse_rf}")
-st.write(f"R-Squared Score: {r2_rf}")
-
-st.write("\n**XGBoost Metrics - Test Set:**")
-st.write(f"Root Mean Squared Error: {rmse_xgb}")
-st.write(f"R-Squared Score: {r2_xgb}")
-
+    # Display prediction
+    st.write("\n**Prediction:**", prediction[0])
