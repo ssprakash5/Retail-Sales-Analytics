39e35c935e97578e080cc713b54621f8aa2fa1da
Sri Surya Prakash S <154003057+ssprakash5@users.noreply.github.com>
2023-12-20 11:24:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,73 +4,57 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
-from sklearn.metrics import mean_squared_error, r2_score
 
-# Load your dataset
-data_df_converted = pd.read_csv("/content/drive/MyDrive/df_converted2.csv")
-
-# Selecting specific columns for modeling
-selected_columns = ['Store', 'Type', 'Size', 'Dept', 'IsHoliday',
-                    'Temperature', 'Fuel_Price', 'MarkDown1', 'MarkDown2',
-                    'MarkDown3', 'MarkDown4', 'MarkDown5', 'CPI', 'Unemployment', 'Weekly_Sales']
-
-data_selected = data_df_converted[selected_columns]
-
-# Splitting into features and target variable
-X = data_selected.drop(columns=['Weekly_Sales'])  # Features
-y = data_selected['Weekly_Sales']  # Target variable
-
-# Splitting the data into train, validation, and test sets
-X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=42)
-X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
-
-# Model Training
-# Decision Tree Model
-dt_model = DecisionTreeRegressor()
-dt_model.fit(X_train, y_train)
-
-# Random Forest Model
-rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
-rf_model.fit(X_train, y_train)
-
-# XGBoost Model
-xgb_model = XGBRegressor()
-xgb_model.fit(X_train, y_train)
+# Function to load data
+@st.cache_data
+def load_data():
+    data_df_converted = pd.read_csv("/content/drive/MyDrive/df_converted2.csv")
+    return data_df_converted
 
 # Streamlit App
-st.title("Weekly Sales Prediction App")
-st.sidebar.header("Input Parameters")
+def main():
+    st.title("Weekly Sales Prediction App")
+    st.sidebar.header("Input Parameters")
 
-# Selectbox and Sliders for user input
-user_input = {}
-for col in X_train.columns:
-    if col in ['Store', 'Type', 'Dept', 'IsHoliday']:
-        unique_values = X_train[col].unique()
-        user_input[col] = st.sidebar.selectbox(col, unique_values)
-    elif col != 'Weekly_Sales':
-        default_val = float(X_train[col].mean())
-        min_val = float(X_train[col].min())
-        max_val = float(X_train[col].max())
-        user_input[col] = st.sidebar.slider(col, min_value=min_val, max_value=max_val, value=default_val)
+    # Load data
+    data_selected = load_data()
 
-# Selecting the model
-selected_model = st.sidebar.selectbox("Select Model", ["Decision Tree", "Random Forest", "XGBoost"])
+    selected_columns = ['Store', 'Type', 'Size', 'Dept', 'IsHoliday',
+                        'Temperature', 'Fuel_Price', 'MarkDown1', 'MarkDown2',
+                        'MarkDown3', 'MarkDown4', 'MarkDown5', 'CPI', 'Unemployment', 'Weekly_Sales']
 
-if selected_model == "Decision Tree":
-    model = dt_model
-elif selected_model == "Random Forest":
-    model = rf_model
-else:
-    model = xgb_model
+    data_selected = data_selected[selected_columns]
 
-# Predict button
-if st.sidebar.button("Predict"):
-    # Convert user input to DataFrame
-    user_input_df = pd.DataFrame([user_input])
-    user_input_df = user_input_df.reindex(columns=X_train.columns, fill_value=0)
+    X = data_selected.drop(columns=['Weekly_Sales'])
+    y = data_selected['Weekly_Sales']
 
-    # Prediction
-    prediction = model.predict(user_input_df)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Display prediction
-    st.write("\n**Prediction:**", prediction[0])
+    user_input = {}
+    for col in X_train.columns:
+        if col in ['Store', 'Type', 'Dept', 'IsHoliday']:
+            unique_values = X_train[col].unique()
+            user_input[col] = st.sidebar.selectbox(col, unique_values)
+        else:
+            default_val = float(X_train[col].mean())
+            min_val = float(X_train[col].min())
+            max_val = float(X_train[col].max())
+            user_input[col] = st.sidebar.slider(col, min_value=min_val, max_value=max_val, value=default_val)
+
+    selected_model = st.sidebar.selectbox("Select Model", ["Decision Tree", "Random Forest", "XGBoost"])
+
+    if selected_model == "Decision Tree":
+        model = DecisionTreeRegressor()
+    elif selected_model == "Random Forest":
+        model = RandomForestRegressor(n_estimators=100, random_state=42)
+    else:
+        model = XGBRegressor()
+
+    if st.sidebar.button("Predict"):
+        model.fit(X_train, y_train)
+        prediction = model.predict(pd.DataFrame([user_input]))
+        st.write("\n**Prediction:**", prediction[0])
+
+if __name__ == "__main__":
+    main()
+
